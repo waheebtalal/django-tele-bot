e26ca3c611685b8e4d7adcc9412bf652515724c1
wahieb <wahebtalal@gmail.com>
2023-05-19 00:58:02 +0300

    last

--- db/settings.py
+++ db/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = ['https://*..railway.app']
